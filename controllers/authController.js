409f3fcbd3db410687ad807dd318bfa2dbef25c5
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-06-13 14:56:43 -0300

    Registration function with validations for password and email.

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,6 +10,45 @@ class AuthController {
     res.render("auth/register");
   }
 
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+
+    if (password != confirmpassword) {
+      req.flash("message", "Password doesn't match");
+      res.render("auth/register");
+
+      return;
+    }
+
+    const checkEmail = await User.findOne({ where: { email: email } });
+
+    if (checkEmail) {
+      req.flash("message", "E-mail already registered.");
+      res.render("auth/register");
+
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+      req.session.userId = createdUser.id;
+      req.flash("message", "Registration completed successfully.");
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = AuthController;
