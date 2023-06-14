e640d283f3db6f403a8309f1a4801366387e40c7
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-06-14 15:27:09 -0300

    Edit routes.

--- routes/ideasRoute.js
+++ routes/ideasRoute.js
@@ -7,6 +7,8 @@ router.post("/createIdea", checkSession, IdeasController.addIdeaSave);
 router.get("/createIdea", checkSession, IdeasController.addIdea);
 router.get("/dashboard-ideas", checkSession, IdeasController.dashboard);
 router.post("/remove-idea", checkSession, IdeasController.removeIdea);
+router.get("/edit/:id", checkSession, IdeasController.editIdea);
+router.post("/edit", checkSession, IdeasController.editIdeaSave);
 router.get("/", IdeasController.showIdeas);
 
 module.exports = router;
